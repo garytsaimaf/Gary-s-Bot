664822054f47863992e939967db2d7f4fa707df4
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 01:18:10 +0800

    Create push.py

---
+++ line/push.py
@@ -0,0 +1,29 @@
+import os
+import requests
+
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+USER_ID = os.getenv("LINE_USER_ID")
+
+
+def push_text(message):
+    url = "https://api.line.me/v2/bot/message/push"
+
+    headers = {
+        "Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+    body = {
+        "to": USER_ID,
+        "messages": [
+            {
+                "type": "text",
+                "text": message
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=body)
+
+    print(response.status_code)
+    print(response.text)
