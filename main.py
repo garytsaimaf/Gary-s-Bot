5b4dc2f0d11ba39951a971e4a2ab8a7207c1b914
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 03:04:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from config.loader import load_topics
 from output.report import build_daily_report
+
 from output.formatter import (
     format_pubmed_section,
     format_google_news_section,
 )
+
 from output.clinicaltrials_formatter import (
     format_clinical_trials_section,
 )
+
 from output.gsk_formatter import (
     format_gsk_section,
+)
+
+from output.fda_formatter import (
+    format_fda_section,
 )
 
 from line.push import push_text
 
 from services.pubmed.search import search_pubmed
 from services.pubmed.fetch import fetch_pubmed_details
+
 from services.google.news import search_google_news
+
 from services.clinicaltrials.search import search_trials
+
 from services.gsk.search import search_gsk_news
+
+from services.fda.search import search_fda_news
 
 
 def main():
@@ -69,6 +81,14 @@ def main():
     gsk_news = search_gsk_news()
 
     results.extend(format_gsk_section(gsk_news))
+
+    # -----------------------
+    # FDA Updates
+    # -----------------------
+
+    fda_news = search_fda_news()
+
+    results.extend(format_fda_section(fda_news))
 
     message = build_daily_report(results)
 
