eb3da33735b291fefbc3f80c3b666e375ca5b1cf
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 21:01:19 +0800

    Update push.py

--- line/push.py
+++ line/push.py
@@ -1,33 +1,33 @@
 import os
 import requests
 
-CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-USER_ID = os.getenv("LINE_USER_ID")
+
+LINE_PUSH_API = "https://api.line.me/v2/bot/message/push"
 
 
 def push_text(message):
-    url = "https://api.line.me/v2/bot/message/push"
+
+    token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+
+    user = os.environ["LINE_USER_ID"]
 
     headers = {
-        "Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}",
-        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
     }
 
-    body = {
-        "to": USER_ID,
+    payload = {
+        "to": user,
         "messages": [
-            {
-                "type": "text",
-                "text": message
-            }
+            message
         ]
     }
 
-    response = requests.post(url, headers=headers, json=body)
+    response = requests.post(
+        LINE_PUSH_API,
+        headers=headers,
+        json=payload
+    )
 
     print(response.status_code)
     print(response.text)
-print("Token length:", len(CHANNEL_ACCESS_TOKEN) if CHANNEL_ACCESS_TOKEN else 0)
-print("Token starts with:", CHANNEL_ACCESS_TOKEN[:10] if CHANNEL_ACCESS_TOKEN else "None")
-print("User ID:", USER_ID)
-print("User ID length:", len(USER_ID) if USER_ID else 0)
