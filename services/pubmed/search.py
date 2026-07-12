4b037a7d7604595bfd9e9097866c633881e3abdb
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 23:27:47 +0800

    Update search.py

--- services/pubmed/search.py
+++ services/pubmed/search.py
@@ -1,16 +1,33 @@
 import requests
 import xml.etree.ElementTree as ET
 
+from config.loader import load_search_config
 
-def search_pubmed(keyword, max_results=5):
 
-    url = (
-        "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
-        f"esearch.fcgi?db=pubmed&term={keyword}"
-        f"&sort=date&retmax={max_results}"
+def search_pubmed(keyword):
+
+    config = load_search_config()["sources"]["pubmed"]
+
+    max_results = config["max_results_per_disease"]
+    period_days = config["period_days"]
+
+    search_term = (
+        f'("{keyword}"[Title/Abstract])'
+        f' AND ("last {period_days} days"[PDat])'
     )
 
-    response = requests.get(url, timeout=30)
+    url = (
+        "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        f"?db=pubmed"
+        f"&term={requests.utils.quote(search_term)}"
+        f"&sort=pub+date"
+        f"&retmax={max_results}"
+    )
+
+    response = requests.get(
+        url,
+        timeout=30
+    )
 
     response.raise_for_status()
 
@@ -19,6 +36,9 @@ def search_pubmed(keyword, max_results=5):
     ids = []
 
     for item in root.findall("IdList/Id"):
-        ids.append(item.text)
+
+        if item.text:
+
+            ids.append(item.text)
 
     return ids
