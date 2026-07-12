5b36b9d6b68bf07cca21622eb2fd7f403a9643a2
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 12:00:55 +0800

    Create gemini.py

---
+++ ai/gemini.py
@@ -0,0 +1,18 @@
+import os
+
+from google import genai
+
+
+client = genai.Client(
+    api_key=os.environ["GEMINI_API_KEY"]
+)
+
+
+def ask_gemini(prompt):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
