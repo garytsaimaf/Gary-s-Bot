ce6d10d6938ff0125ddc1b28e5983ebc171fa354
garytsaimaf <garytsai.maf@gmail.com>
2026-07-12 12:43:05 +0800

    Update gemini.py

--- ai/gemini.py
+++ ai/gemini.py
@@ -8,17 +8,19 @@ def ask_gemini(prompt):
     api_key = os.getenv("GEMINI_API_KEY")
 
     if not api_key:
+        raise RuntimeError("GEMINI_API_KEY is missing.")
 
-        raise RuntimeError(
-            "GEMINI_API_KEY is missing."
-        )
+    model = os.getenv(
+        "GEMINI_MODEL",
+        "gemini-2.5-flash-lite"
+    )
 
     client = genai.Client(
         api_key=api_key
     )
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model=model,
         contents=prompt,
     )
 
